904eb09f8680b2e790eb81fe2597eb4a45973c8f
Michael Banck <michael.banck@credativ.de>
2024-09-20 20:18:36 +0200

    Release version 1.2

--- pg_checksums_ext.c
+++ pg_checksums_ext.c
@@ -11,7 +11,7 @@
  *-------------------------------------------------------------------------
  */
 
-#define PG_CHECKSUMS_VERSION "1.1"
+#define PG_CHECKSUMS_VERSION "1.2"
 
 #include "postgres_fe.h"
 
